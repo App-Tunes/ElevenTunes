7069c4acce6ae44130de5671561432928a593afc
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-12-28 01:27:47 +0100

    Remove mock playlists from default library.

--- ElevenTunes/Document/DocumentController.swift
+++ ElevenTunes/Document/DocumentController.swift
@@ -10,10 +10,10 @@ import AppKit
 class DocumentController: NSDocumentController {
     override func makeUntitledDocument(ofType typeName: String) throws -> NSDocument {
         let doc = LibraryDocument()
-        doc.library.mainPlaylist.add(children: [
-            LibraryMock.playlist("Playlist 1"),
-            LibraryMock.playlist("Playlist 2")
-        ])
+//        doc.library.mainPlaylist.add(children: [
+//            LibraryMock.playlist("Playlist 1"),
+//            LibraryMock.playlist("Playlist 2")
+//        ])
         return doc
     }
 }
