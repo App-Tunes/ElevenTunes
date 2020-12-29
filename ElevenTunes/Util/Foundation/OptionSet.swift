d5b6dd139af69a6ec3de6ecf2836778896199e12
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-12-29 12:21:07 +0100

    Add OptionSet extension for bit components.

---
+++ ElevenTunes/Util/Foundation/OptionSet.swift
@@ -0,0 +1,22 @@
+//
+//  OptionSet.swift
+//  ElevenTunes
+//
+//  Created by Lukas Tenbrink on 29.12.20.
+//
+
+import Foundation
+
+extension FixedWidthInteger {
+    init(bitComponents : [Self]) {
+        self = bitComponents.reduce(0, +)
+    }
+
+    var bitComponents : [Self] {
+        (0 ..< Self.bitWidth).map { 1 << $0 } .filter { self & $0 != 0 }
+    }
+}
+
+extension OptionSet where RawValue: FixedWidthInteger, Self == Self.Element {
+    var components : [Self] { rawValue.bitComponents.map(Self.init) }
+}
