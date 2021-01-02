57f1fb2bcf465e7c04d112f0f18196e2e3cb40a3
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2021-01-02 22:44:53 +0100

    Add some more info to playlist bar.

--- ElevenTunes/Views/Playlist/PlaylistBarView.swift
+++ ElevenTunes/Views/Playlist/PlaylistBarView.swift
@@ -9,19 +9,43 @@ import SwiftUI
 
 struct PlaylistBarView: View {
     let playlist: Playlist
+    @State var contentMask: PlaylistContentMask = []
     @State var tracks: [AnyTrack] = []
+    @State var attributes: TypedDict<PlaylistAttribute> = .init()
 
     var body: some View {
-        GeometryReader { geo in
-            Text("\(tracks.count) tracks")
-                .position(x: geo.size.width / 2, y: geo.size.height / 2 - 5)
+        HStack {
+            Spacer()
+                .frame(width: 8)
+
+            // TODO Make draggable
+            playlist.backend.icon
+                .foregroundColor(playlist.backend.accentColor)
+            
+            Text(attributes[PlaylistAttribute.title] ?? "...")
                 .foregroundColor(.secondary)
+
+            Spacer()
+
+            Text("\(tracks.count) tracks")
+                .foregroundColor(.secondary)
+            
+            Button {
+                // TODO View playlist info
+            } label: {
+                Image(systemName: "gearshape")
+            }
+            .buttonStyle(BorderlessButtonStyle())
+            .disabled(true)
+
+            Spacer()
+                .frame(width: 8)
         }
             .frame(minWidth: 200)
-            .padding(.top, 6)
             .frame(height: 25)
             .visualEffectBackground(material: .headerView, blendingMode: .withinWindow)
             .onReceive(playlist.backend.tracks()) { tracks = $0 }
+            .onReceive(playlist.backend.attributes()) { attributes = $0 }
     }
 }
 
