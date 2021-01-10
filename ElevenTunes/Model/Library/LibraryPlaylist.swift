db15ed7f4790b341ddbeef09d282699e61bb4e6d
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2021-01-11 00:16:03 +0100

    Fix Library not loading children and tracks.

--- ElevenTunes/Model/Library/LibraryPlaylist.swift
+++ ElevenTunes/Model/Library/LibraryPlaylist.swift
@@ -21,38 +21,54 @@ class LibraryPlaylist: AnyPlaylist {
         
 	let _attributes: VolatileAttributes<PlaylistAttribute, PlaylistVersion> = .init()
 	
+	var tracksObserver: AnyCancellable?
+	var childrenObserver: AnyCancellable?
+
     init(library: Library, playContext: PlayContext) {
         self.managedObjectContext = library.managedObjectContext
         self.playContext = playContext
         self.library = library
         
-		// TODO
-//        _tracks = CDPublisher(request: DBTrack.createFetchRequest(), context: managedObjectContext)
-//            // TODO Apparently, sometimes the same object is emitted twice
-//            .map { $0.removeDuplicates() }
-//            .removeDuplicates()
-//            .flatMap {
-//                $0.map { [weak self] in self!.library!.track(cachedBy: $0) }
-//                    .combineLatest()
-//            }
-//            .replaceError(with: [])
-//            .map { TracksSnapshot($0) }
-//            .eraseToAnyPublisher()
-//
-//        _children = CDPublisher(request: LibraryPlaylist.playlistFetchRequest, context: managedObjectContext)
-//            // TODO Apparently, sometimes the same object is emitted twice
-//            .map { $0.removeDuplicates { $0.uuid } }
-//            .removeDuplicates()
-//            .flatMap {
-//                $0
-//                    .map { [weak self] in self!.library!.playlist(cachedBy: $0) }
-//                    .combineLatest()
-//            }
-//            .combineLatest($staticPlaylists.eraseError()).map { $1 + $0 }
-//            .replaceError(with: [])
-//            .map { PlaylistsSnapshot($0) }
-//            .eraseToAnyPublisher()
-                
+        tracksObserver = CDPublisher(request: DBTrack.createFetchRequest(), context: managedObjectContext)
+            // TODO Apparently, sometimes the same object is emitted twice
+            .map { $0.removeDuplicates() }
+            .removeDuplicates()
+            .flatMap {
+                $0.map { [weak self] in self!.library!.track(cachedBy: $0) }
+                    .combineLatest()
+            }
+			.sink(receiveResult: { [weak self] result in
+				switch result {
+				case .failure(let error):
+					self?._attributes.updateEmpty([.tracks], state: .error(error))
+				case .success(let tracks):
+					self?._attributes.update(.init([
+						.tracks: tracks
+					]), state: .version(""))
+				}
+			})
+
+        childrenObserver = CDPublisher(request: LibraryPlaylist.playlistFetchRequest, context: managedObjectContext)
+            // TODO Apparently, sometimes the same object is emitted twice
+            .map { $0.removeDuplicates { $0.uuid } }
+            .removeDuplicates()
+            .flatMap {
+                $0
+                    .map { [weak self] in self!.library!.playlist(cachedBy: $0) }
+                    .combineLatest()
+            }
+            .combineLatest($staticPlaylists.eraseError()).map { $1 + $0 }
+			.sink(receiveResult: { [weak self] result in
+				switch result {
+				case .failure(let error):
+					self?._attributes.updateEmpty([.children], state: .error(error))
+				case .success(let children):
+					self?._attributes.update(.init([
+						.children: children
+					]), state: .version(""))
+				}
+			})
+
         staticPlaylists = [
             SpotifyUser(spotify: library.spotify)
         ]
