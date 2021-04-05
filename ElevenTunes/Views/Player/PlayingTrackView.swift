07b9d86fd1ba6002325000d0b388fee4802ad808
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2021-04-05 02:09:34 +0200

    Separate PlayingTrackView from its contents ("just" a nullable cell).

--- ElevenTunes/Views/Player/PlayingTrackView.swift
+++ ElevenTunes/Views/Player/PlayingTrackView.swift
@@ -10,51 +10,66 @@ import Foundation
 import SwiftUI
 import Combine
 
-struct PlayingTrackView: View {
-    @State var player: Player
-
-    @State var current: Track?
-    @State var attributes: TypedDict<TrackAttribute> = .init()
-
+struct FullTrackCellView: View {
+	@State var track: Track?
+	
 	@ViewBuilder var contextMenu: some View {
-		if let current = current {
-			TrackActions(tracks: [current])()
+		if let track = track {
+			TrackActions(tracks: [track])()
 		}
 	}
-	
-    var body: some View {
-        HStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.black)
-                    .opacity(0.2)
-                
-				TrackImageView(track: current)
-            }
-			.contextMenu(menuItems: { self.contextMenu })
-            .frame(width: 28, height: 28)
-            .cornerRadius(5)
 
-            if let current = current {
-                TrackCellView(track: current)
+	var body: some View {
+		HStack {
+			ZStack {
+				Rectangle()
+					.fill(Color.black)
+					.opacity(0.2)
+				
+				TrackImageView(track: track)
+			}
+			.contextMenu(menuItems: { self.contextMenu })
+			.frame(width: 28, height: 28)
+			.cornerRadius(5)
+
+			if let track = track {
+				TrackCellView(track: track)
 				
 				Spacer()
 				
-				TrackTempoView(track: current)
+				TrackTempoView(track: track)
 				
-				TrackKeyView(track: current)
+				TrackKeyView(track: track)
 					.padding(.horizontal)
 				
-				TrackDurationView(track: current)
+				TrackDurationView(track: track)
 					.frame(width: 60)
-            }
-            else {
-                Text("Nothing Playing").opacity(0.5)
-            }
-        }
-		.frame(height: 28)
-        .padding(.top, 8)
-        .onReceive(player.$current) { self.current = Track($0) }
-		.id(current?.id)
+			}
+			else {
+				Text("Nothing Playing").opacity(0.5)
+				
+				Spacer()
+			}
+		}
+	}
+}
+
+struct PlayingTrackView: View {
+    @State var player: Player
+
+	@State var current: Track?
+
+    var body: some View {
+		FullTrackCellView(track: current)
+			.frame(height: 28)
+			.padding(.top, 8)
+			.onReceive(player.$current) { newTrack in
+				guard newTrack?.id != current?.id else {
+					return
+				}
+				
+				self.current = Track(newTrack)
+			}
+			.id(current?.id)
     }
 }
