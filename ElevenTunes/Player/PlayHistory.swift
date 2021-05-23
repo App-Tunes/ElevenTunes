6f2a99c6338a089e410540ecee3a3117801132d0
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2021-05-23 15:32:28 +0200

    Decouple knowledge of item type from History class.

--- ElevenTunes/Player/PlayHistory.swift
+++ ElevenTunes/Player/PlayHistory.swift
@@ -8,47 +8,27 @@
 import Foundation
 import Combine
 
-enum PlayHistoryContext {
-    case playlist(_ playlist: AnyPlaylist, tracks: [AnyTrack], track: AnyTrack)
-	
-	var fromStart: PlayHistoryContext {
-		switch self {
-		case .playlist(let playlist, let tracks, _):
-			return .playlist(playlist, tracks: tracks, track: tracks[0])
-		}
-	}
-}
-
-class PlayHistory {
+class History<Item> {
 	var context: PlayHistoryContext?
 	
-    @Published private(set) var queue: [AnyTrack]
-    @Published private(set) var history: [AnyTrack] = []
+    @Published private(set) var queue: [Item]
+    @Published private(set) var history: [Item] = []
 
     // previous = history.last, next = queue.first
-    @Published private(set) var previous: AnyTrack?
-    @Published private(set) var current: AnyTrack?
-    @Published private(set) var next: AnyTrack?
+    @Published private(set) var previous: Item?
+    @Published private(set) var current: Item?
+    @Published private(set) var next: Item?
     
-    init(_ queue: [AnyTrack] = [], history: [AnyTrack] = []) {
+    init(_ queue: [Item] = [], history: [Item] = []) {
         self.queue = queue
         self.history = history
         
         $queue.map(\.first).assign(to: &$next)
         $history.map(\.last).assign(to: &$previous)
     }
-    
-    convenience init(context: PlayHistoryContext) {
-        switch context {
-        case .playlist(_, let tracks, let track):
-			let index = tracks.firstIndex { $0.id == track.id } ?? tracks.count
-            self.init(Array(tracks[index...]), history: Array(tracks[..<index]))
-        }
-		self.context = context
-    }
-	        
+    	        
     @discardableResult
-    func forwards() -> AnyTrack? {
+    func forwards() -> Item? {
         // Move forwards in replacements so that no value is missing at any point
         if let current = current { history.append(current) }
         current = queue.first
@@ -58,7 +38,7 @@ class PlayHistory {
     }
     
     @discardableResult
-    func backwards() -> AnyTrack? {
+    func backwards() -> Item? {
         // Move backwards in replacements so that no value is missing at any point
         if let current = current { queue.prepend(current) }
         current = history.last
@@ -66,4 +46,28 @@ class PlayHistory {
         
         return current
     }
+}
+
+typealias PlayHistory = History<AnyTrack>
+
+enum PlayHistoryContext {
+	case playlist(_ playlist: AnyPlaylist, tracks: [AnyTrack], track: AnyTrack)
+	
+	var fromStart: PlayHistoryContext {
+		switch self {
+		case .playlist(let playlist, let tracks, _):
+			return .playlist(playlist, tracks: tracks, track: tracks[0])
+		}
+	}
+}
+
+extension PlayHistory {
+	convenience init(context: PlayHistoryContext) {
+		switch context {
+		case .playlist(_, let tracks, let track):
+			let index = tracks.firstIndex { $0.id == track.id } ?? tracks.count
+			self.init(Array(tracks[index...]), history: Array(tracks[..<index]))
+		}
+		self.context = context
+	}
 }
